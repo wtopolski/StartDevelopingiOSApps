1c3426b004cc43571444fa5cf9da40e7d8417a4a
Wojciech Topolski <wojciechtopolski@wojciech-topolski.local>
2016-01-04 22:41:24 +0100

    Define Your Data Model

--- SampleAppTests/SampleAppTests.swift
+++ SampleAppTests/SampleAppTests.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2015 Wojciech Topolski. All rights reserved.
 //
 
+import UIKit
 import XCTest
 @testable import SampleApp
 
@@ -23,14 +24,28 @@ class SampleAppTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true, "Pass")
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measureBlock() {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    // Tests to confirm that the Meal initializer returns when no name or a negative rating is provided.
+    func testMealInitialization() {
+        // Success case.
+        let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem)
+        
+        // Failure cases.
+        let noName = Meal(name: "", photo: nil, rating: 0)
+        XCTAssertNil(noName, "Empty name is invalid")
+        
+        let badRating = Meal(name: "Really bad rating", photo: nil, rating: -1)
+        XCTAssertNil(badRating)
     }
     
 }
