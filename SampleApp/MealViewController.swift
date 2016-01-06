2b444fb228cef2bf6e35e0dbbd7cb449d0901cfd
Wojciech Topolski <wojciechtopolski@Wojciech-Topolski.local>
2016-01-06 18:34:50 +0100

    Create a Table View

--- SampleApp/MealViewController.swift
+++ SampleApp/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  SampleApp
 //
 //  Created by Wojciech Topolski on 23.12.2015.
@@ -8,10 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var nameLabelField: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -53,7 +52,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
-        nameLabelField.text = textField.text
     }
     
     // MARK: UIImagePickerControllerDelegate
